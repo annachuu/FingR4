b59b8b8d529aecdbe7a6b3f176f4f9dc76657d79
anna <annachu211@gmail.com>
2025-11-30 21:15:58 -0600

    fixed distance text

--- mainWithUltraSonicAndWave.ino
+++ mainWithUltraSonicAndWave.ino
@@ -88,7 +88,7 @@ float getDistance()
     Serial.println("No echo detected");
   }
   float distance = (duration * 0.034) / 2;
-  Serial.print(Distance: );
+  Serial.print("Distance: ");
   Serial.print(distance);
   Serial.print(" cm");
   // return distance;
